6816c438f78e41b0e08df0e990d12e0114415bc7
hiiaagooo <hiagoforte1@hotmail.com>
2018-06-04 01:04:44 -0300

    ajustes

--- public/scripts/pacientes.controller.js
+++ public/scripts/pacientes.controller.js
@@ -21,27 +21,22 @@
       		nome: $scope.novoPaciente.nome,
       		sexo: $scope.novoPaciente.sexo,
       		idade: $scope.novoPaciente.idade,
-			endereco: $scope.novoPaciente.endereco,
-			sintomas: $scope.novoPaciente.sintomas,
+					endereco: $scope.novoPaciente.endereco,
+					sintomas: $scope.novoPaciente.sintomas,
       		medico: $scope.novoPaciente.medico
       	};
 
-		MeusFilmes.inserir(filme).then((carregarPacientes) => {
-			debugger;
-            $scope.novoPaciente = carregarPacientes;
-		});
-
+		MeusFilmes.inserir(filme).then(carregarPacientes);
       	$scope.novoPaciente = {};
 	  }
-	  
-
-      $scope.removerFilme = function(id) {
-      	angular.forEach($scope.filmes, function(filme, i){
-      		if(filme.id == id){
-      			$scope.filmes.splice(i, 1);
-      		};
-      	});
-      }
+	   
+		$scope.removerFilme = function(id) {
+			angular.forEach($scope.filmes, function(filme, i){
+				if(filme.id == id){
+					$scope.filmes.splice(i, 1);
+				};
+			});
+		}
 
 	  carregarPacientes();
 
