3f3de56d97fea6473773636f4cb7944ed53b2406
hiiaagooo <hiagoforte1@hotmail.com>
2018-06-05 02:12:12 -0300

    ajusteAlert

--- public/scripts/pacientes.controller.js
+++ public/scripts/pacientes.controller.js
@@ -13,43 +13,46 @@
 		});
 	  }
 
-      $scope.novoPaciente = {};
-
-			$scope.resetForm = function() {
-				$scope.formulario.$setPristine();
-				$scope.formulario.$setUntouched();
-			}
-
-      $scope.criarPaciente = function() {
-				$scope.formulario.$setDirty();
-
-				if ($scope.formulario.$invalid)
-					return;
-
-      	var filme = {
-      		id: Date.now() + "",
-      		nome: $scope.novoPaciente.nome,
-      		sexo: $scope.novoPaciente.sexo,
-      		idade: $scope.novoPaciente.idade,
-					endereco: $scope.novoPaciente.endereco,
-					sintomas: $scope.novoPaciente.sintomas,
-      		medico: $scope.novoPaciente.medico
-      	};
-
-		MeusFilmes.inserir(filme).then(carregarPacientes);
-      	$scope.novoPaciente = {};
-	  }
-	   
-		$scope.removerFilme = function(id) {
-			angular.forEach($scope.filmes, function(filme, i){
-				if(filme.id == id){
-					$scope.filmes.splice(i, 1);
-				};
-			});
+		$scope.novoPaciente = {};
+			
+		$scope.resetForm = function() {
+			$scope.formulario.$setPristine();
+			$scope.formulario.$setUntouched();
 		}
 
-	  carregarPacientes();
+		$scope.criarPaciente = function() {
+			$scope.formulario.$setDirty();
 
+			if ($scope.formulario.$invalid)
+				return;
+
+			var filme = {
+				id: Date.now() + "",
+				nome: $scope.novoPaciente.nome,
+				sexo: $scope.novoPaciente.sexo,
+				idade: $scope.novoPaciente.idade,
+				endereco: $scope.novoPaciente.endereco,
+				sintomas: $scope.novoPaciente.sintomas,
+				medico: $scope.novoPaciente.medico
+			};
+
+	MeusFilmes.inserir(filme).then(carregarPacientes);
+			alert('Obrigado!');
+			$scope.novoPaciente = {};
+			$scope.resetForm();
+	}
+	 
+	$scope.removerFilme = function(id) {
+		angular.forEach($scope.filmes, function(filme, i){
+			if(filme.id == id){
+				$scope.filmes.splice(i, 1);
+				alert('Paciente excluído!');
+			};
 		});
+	}
+	carregarPacientes();
 	
+	});
+
 })();
+
